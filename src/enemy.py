19fea598ea04acb50d19d76b11c736b2a55940f0
GusInfiniteLinesOfCode <101442959+GusInfiniteLinesOfCode@users.noreply.github.com>
2023-10-07 11:07:13 -0400

    added enemy stuff

--- src/enemy.py
+++ src/enemy.py
@@ -1,5 +1,6 @@
 import pygame as pig
 import random
+import math
 
 
 class enemy:
@@ -10,19 +11,23 @@ class enemy:
         health=random.randint(3, 7),
         damaged=False,
         defence=random.randint(1, 4),
+        passive=False,
     ):
         self.enemyy = enemyy
         self.enemyx = enemyx
         self.health = health
         self.damaged = damaged
         self.defence = defence
+        self.passive = passive
 
     def roundtoblock(self, x, base=15):
         return base * round(x / base)
 
     def update(self, x, y, colliders, sky):
         self.move(x, colliders, sky)
-        if self.enemyx == x and self.enemyy == y:
+        # Calculate the distance from the player to the enemy
+        distance = math.sqrt((self.enemyx - x) ** 2 + (self.enemyy - y) ** 2)
+        if distance <= 30:  # two blocks
             self.attack()
         if self.damaged:
             self.attacked(None, None)
@@ -34,24 +39,30 @@ class enemy:
         pass
 
     def move(self, x, colliders, sky):
-        if any(block.collidepoint(self.enemyx, self.enemyy) for block in colliders):
-            if not any(
-                block.collidepoint(self.enemyx, (self.roundtoblock(self.enemyy) - 16))
-                for block in colliders
-            ):
-                self.enemyy -= 5
-                if self.enemyx <= x:
-                    self.enemyx += 1
+        if not self.passive:
+            if any(block.collidepoint(self.enemyx, self.enemyy) for block in colliders):
+                if not any(
+                    block.collidepoint(
+                        self.enemyx, (self.roundtoblock(self.enemyy) - 16)
+                    )
+                    for block in colliders
+                ):
+                    self.enemyy -= 5
+                    if self.enemyx <= x:
+                        self.enemyx += 1
+                    else:
+                        self.enemyx -= 1
                 else:
-                    self.enemyx -= 1
-            else:
-                self.enemyx = self.roundtoblock(self.enemyx)
-                if self.enemyx <= x:
-                    self.enemyx -= 1
-                else:
-                    self.enemyx += 1
-        if any(air.collidepoint(self.enemyx, self.enemyy + 3) for air in sky):
-            self.enemyy += 5
+                    self.enemyx = self.roundtoblock(self.enemyx)
+                    if self.enemyx <= x:
+                        self.enemyx -= 1
+                    else:
+                        self.enemyx += 1
+            if any(air.collidepoint(self.enemyx, self.enemyy + 3) for air in sky):
+                self.enemyy += 5
+        else:
+            # implement passive logic
+            pass
 
     def draw(self, screen):
         pig.draw.rect(screen, (200, 0, 0), (self.enemyx - 10, self.enemyy - 15, 20, 20))
