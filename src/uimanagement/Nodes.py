18da1f97d73b11124238c0233e3e49aed517ff57
GusInfiniteLinesOfCode <101442959+GusInfiniteLinesOfCode@users.noreply.github.com>
2023-10-28 11:25:37 -0400

    Update Nodes.py

--- src/uimanagement/Nodes.py
+++ src/uimanagement/Nodes.py
@@ -37,6 +37,21 @@ end_node = {"label": "End", "type": "End", "position": (WIDTH - 50, HEIGHT // 2)
 nodes.append(start_node)
 nodes.append(end_node)
 
+# Constants for the inspector tab
+INSPECTOR_WIDTH = 200
+INSPECTOR_COLOR = (50, 50, 50)
+
+# Create an Inspector rect
+inspector_rect = pygame.Rect(WIDTH - INSPECTOR_WIDTH, 0, INSPECTOR_WIDTH, HEIGHT)
+
+# Create a text input area
+text_input_rect = pygame.Rect(
+    WIDTH - INSPECTOR_WIDTH + 10, 50, INSPECTOR_WIDTH - 20, 100
+)
+text_input_text = ""
+font = pygame.font.Font(None, 36)
+text_input_active = False
+
 
 # Function to add a new node to the canvas
 def add_node(node_type, position):
@@ -54,6 +69,30 @@ def add_connection(start_node, end_node):
     connections.append((start_node, end_node))
 
 
+# Function to draw the inspector tab
+def draw_inspector():
+    pygame.draw.rect(screen, INSPECTOR_COLOR, inspector_rect)
+
+
+# Function to display details about the selected node in the inspector tab
+def show_inspector(node):
+    if node is not None:
+        # Create a text input box for details
+        pygame.draw.rect(screen, (255, 255, 255), text_input_rect)
+        pygame.draw.rect(screen, (0, 0, 0), text_input_rect, 2)
+
+        # Display details about the selected node
+        if "type" in node:
+            details = "Node Type: " + node["type"]
+            font = pygame.font.Font(None, 28)
+            text = font.render(details, True, (0, 0, 0))
+            text_rect = text.get_rect()
+            text_rect.topleft = (text_input_rect.x + 10, text_input_rect.y + 10)
+            screen.blit(text, text_rect)
+        else:
+            text_input_text = ""
+
+
 # Main loop
 running = True
 selected_node = None
@@ -61,12 +100,10 @@ adding_node = None
 dragging_node = None
 mouse_dragging = False
 connecting_node = None
-# Additional variable to track line drawing
 drawing_line = False
 line_start = None
-# Additional variables for node movement and direction drawing
 node_moving = False
-directions = {}  # Dictionary to store directions for each connection
+directions = {}
 
 while running:
     for event in pygame.event.get():
@@ -74,35 +111,39 @@ while running:
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:  # Left mouse button
+            if event.button == 1:
                 pos = pygame.mouse.get_pos()
                 if adding_node is not None:
                     add_node(adding_node, pos)
                     adding_node = None
+
+                # Check if a node is clicked and make it the selected node
                 for node in nodes:
                     if node["rect"].collidepoint(pos):
                         selected_node = node
                         dragging_node = node
+
                 for idx, node_type in enumerate(node_types):
                     node_rect = pygame.Rect(10, 10 + idx * 30, 200, 20)
                     if node_rect.collidepoint(pos):
                         adding_node = node_type
 
-                # Check for starting a connection
                 for node in nodes:
                     if node["rect"].collidepoint(pos):
                         connecting_node = node
                         drawing_line = True
                         line_start = connecting_node["position"]
 
-            elif event.button == 2:  # Middle mouse button (scroll wheel)
+            elif event.button == 2:
                 pos = pygame.mouse.get_pos()
                 for node in nodes:
                     if node["rect"].collidepoint(pos):
                         selected_node = node
                         dragging_node = node
                         mouse_dragging = True
-            if event.button == 3:  # Right mouse button
+                text_input_active = text_input_rect.collidepoint(pos)
+
+            if event.button == 3:
                 pos = pygame.mouse.get_pos()
                 for node in nodes:
                     if node["rect"].collidepoint(pos):
@@ -110,9 +151,8 @@ while running:
                         node_moving = True
                         break
 
-        # Check for completing a connection
         if event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:  # Left mouse button
+            if event.button == 1:
                 if drawing_line:
                     pos = pygame.mouse.get_pos()
                     for node in nodes:
@@ -121,32 +161,31 @@ while running:
                     drawing_line = False
                 connecting_node = None
 
-            if event.button == 1:  # Left mouse button
+            if event.button == 1:
                 if dragging_node and selected_node:
                     selected_node = None
                     dragging_node = None
 
-            elif event.button == 2:  # Middle mouse button (scroll wheel)
+            elif event.button == 2:
                 selected_node = None
                 dragging_node = None
                 mouse_dragging = False
 
     screen.fill((0, 0, 0))
 
-    # Draw connections
     for connection in connections:
         start_pos = connection[0]["position"]
         end_pos = connection[1]["position"]
         pygame.draw.line(screen, LINE_COLOR, start_pos, end_pos, CONNECTION_WIDTH)
 
-    # Draw start and end nodes
     for node in [start_node, end_node]:
         node["rect"] = pygame.draw.circle(
             screen, NODE_COLOR, node["position"], NODE_RADIUS
         )
+        if node == selected_node:
+            pygame.draw.circle(screen, (0, 255, 0), node["position"], NODE_RADIUS)
         pygame.draw.circle(screen, LINE_COLOR, node["position"], NODE_RADIUS, 2)
 
-    # Draw node types and labels
     for idx, node_type in enumerate(node_types):
         node = node_type["label"]
         node_rect = pygame.Rect(10, 10 + idx * 30, 200, 20)
@@ -155,11 +194,12 @@ while running:
         text = font.render(node, True, (255, 255, 255))
         screen.blit(text, (20, 20 + idx * 30))
 
-    # Draw nodes and their names in blue
     for node in nodes:
         node["rect"] = pygame.draw.circle(
             screen, NODE_COLOR, node["position"], NODE_RADIUS
         )
+        if node == selected_node:
+            pygame.draw.circle(screen, (0, 255, 0), node["position"], NODE_RADIUS)
         pygame.draw.circle(screen, LINE_COLOR, node["position"], NODE_RADIUS, 2)
         font = pygame.font.Font(None, 24)
         text = font.render(node["type"], True, TEXT_COLOR)
@@ -167,7 +207,6 @@ while running:
         text_rect.center = (node["position"][0], node["position"][1] - NODE_RADIUS - 10)
         screen.blit(text, text_rect)
 
-    # Draw the line while drawing_line is True
     if drawing_line:
         pygame.draw.line(
             screen,
@@ -176,6 +215,12 @@ while running:
             pygame.mouse.get_pos(),
             CONNECTION_WIDTH,
         )
+
+    # Draw the inspector tab
+    draw_inspector()
+
+    # Show options for the selected node in the inspector tab
+    show_inspector(selected_node)
 
     pygame.display.flip()
 
